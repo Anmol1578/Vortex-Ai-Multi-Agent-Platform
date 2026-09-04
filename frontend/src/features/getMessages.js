c0a505dfd16a7619278833ff62d3edceadb91722
Anmol1578 <anmol10562@gmail.com>
2026-09-04 11:16:23 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/src/features/getMessages.js
+++ frontend/src/features/getMessages.js
@@ -1,18 +1,3 @@
-// import React from "react";
-// import api from "../../utils/axios";
-// async function getMessages(id) {
-//   try {
-//     const { data } = await api.get(`/api/chat/get-messages/${id}`);
-//    console.log(data)
-//    return data
-//   } catch (error) {
-//     console.log(error)
-//     return []
-//   }
-// }
-
-// export default getMessages;
-
 import api from "../../utils/axios";
 
 async function getMessages(id) {
@@ -22,7 +7,7 @@ async function getMessages(id) {
         "Cache-Control": "no-cache",
       },
     });
-    
+
     return data;
   } catch (error) {
     console.log("Get messages error:", error);
